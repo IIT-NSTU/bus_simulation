cf3c5339147e5fe3c25b84e0651456dc32f6c342
Imran Hasan <imran258hasan@gmail.com>
2023-08-30 11:49:10 +0600

    Update BusSimulation.java

--- code/BusSimulation.java
+++ code/BusSimulation.java
@@ -1,93 +1,80 @@
 import java.util.Scanner;
-
-public class BusSimulation {
-    int ROWS = 20;
-    int COLUMNS = 100;
-    char[][] board_box = new char[ROWS][COLUMNS];
+public class BusSimulation 
+{
+    int row = 20;
+    int col = 100;
+    char[][] board_box = new char[row][col];
     int stopage_size, distance_size;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner get_input = new Scanner(System.in);
-        BusSimulation busSimulation_object = new BusSimulation();
-        Board board_object = new Board();
-        Stopage stopage_object = new Stopage();
-        Distance distance_object = new Distance();
-
-        board_object.initializeBoard(busSimulation_object);
-        board_object.printBoard(busSimulation_object);
-
-        Stopage[] stopageArray = stopage_object.getStopage(busSimulation_object);
-
+        BusSimulation bus_obj = new BusSimulation();
+        Board b_obj = new Board();
+        Stopage s_obj = new Stopage();
+        Distance dist_obj = new Distance();
+        b_obj.initialize_board(bus_obj);
+        b_obj.print_board(bus_obj);
+        Stopage[] stopageArray = s_obj.getStopage(bus_obj);
         System.out.print("\nAll stopage symbol:");
-        for (Stopage stopage : stopageArray) {
+        for (Stopage stopage : stopageArray) 
+        {
             System.out.print(" " + stopage.pos_symbol);
         }
         System.out.print("\n\n");
-
-        board_object.printBoard(busSimulation_object);
-
-        Distance[] distanceArray = distance_object.getDistance(busSimulation_object);
-
-        BellmanFord graph = new BellmanFord(busSimulation_object.stopage_size, busSimulation_object.distance_size);
-        for (Distance distance : distanceArray) {
-            graph.addEdge(distance.Start, distance.End, (int)distance.Weight);
+        b_obj.print_board(bus_obj);
+        Distance[] dist_arr = dist_obj.get_dist(bus_obj);
+        BellmanFord graph = new BellmanFord(bus_obj.stopage_size, bus_obj.distance_size);
+        for (Distance distance : dist_arr) 
+        {
+            graph.addEdge(distance.Start, distance.End,distance.Weight);
         }
-
         char source, destination;
-        System.out.print("Enter Source Node: ");
+        System.out.print("Enter Source Node:");
         source = get_input.nextLine().charAt(0);
         System.out.print("Enter Destination Node: ");
         destination = get_input.nextLine().charAt(0);
-
-        // Find shortest path and mark the board with the path
         graph.shortestPath(source, destination);
-
         System.out.println("\nShortest path from " + source + " to " + destination + ": ");
-        markPath(busSimulation_object, source, destination);
-        board_object.printBoard(busSimulation_object);
+        markPath(bus_obj, source, destination);
+        b_obj.print_board(bus_obj);
     }
-
-    public static void markPath(BusSimulation busSimulation, char source, char destination) {
-        int sourceRow = -1, sourceCol = -1;
-        int destinationRow = -1, destinationCol = -1;
-
-        // Find the coordinates of the source and destination
-        for (int row = 0; row < busSimulation.ROWS; row++) {
-            for (int col = 0; col < busSimulation.COLUMNS; col++) {
-                if (busSimulation.board_box[row][col] == source) {
-                    sourceRow = row;
-                    sourceCol = col;
-                } else if (busSimulation.board_box[row][col] == destination) {
-                    destinationRow = row;
-                    destinationCol = col;
+    public static void markPath(BusSimulation busSimulation, char source, char destination) 
+    {
+        int sourc_row = -1, sourc_col = -1;
+        int dest_row = -1, dest_col = -1;
+        for (int i = 0; i < busSimulation.row; i++) 
+        {
+            for (int j = 0; j < busSimulation.col; j++) 
+            {
+                if(busSimulation.board_box[i][j] == source) 
+                {
+                    sourc_row = i;
+                    sourc_col = j;
+                } 
+                else if(busSimulation.board_box[i][j]==destination) 
+                {
+                    dest_row = i;
+                    dest_col = j;
                 }
             }
         }
-
-        // Mark the cells along the path with the '*' symbol
-        int currentRow = sourceRow, currentCol = sourceCol;
-        while (currentRow != destinationRow || currentCol != destinationCol) {
-            char cell = busSimulation.board_box[currentRow][currentCol];
-            if (cell != source && cell != destination && !isStopage(cell)) {
-                busSimulation.board_box[currentRow][currentCol] = '*';
+        int curr_row = sourc_row, curr_col = sourc_col;
+        while (curr_row != dest_row || curr_col != dest_col) 
+        {
+            char cell = busSimulation.board_box[curr_row][curr_col];
+            if (cell != source && cell != destination && !is_stopage(cell)) 
+            {
+                busSimulation.board_box[curr_row][curr_col] = '*';
             }
-
-            // Move towards the destination
-            if (currentRow < destinationRow) currentRow++;
-            else if (currentRow > destinationRow) currentRow--;
-
-            if (currentCol < destinationCol) currentCol++;
-            else if (currentCol > destinationCol) currentCol--;
+            if (curr_row < dest_row) curr_row++;
+            else if (curr_row > dest_row) curr_row--;
+            if (curr_col < dest_col) curr_col++;
+            else if (curr_col > dest_col) curr_col--;
         }
-        char cell = busSimulation.board_box[currentRow][currentCol];
-        if (cell != source && cell != destination && !isStopage(cell)) {
-            busSimulation.board_box[currentRow][currentCol] = '*';
-        }
+        
     }
-
-    public static boolean isStopage(char c) {
-        // You can modify this method to check if a character is a stopage character
-        // For now, it checks if the character is not a space
+    public static boolean is_stopage(char c) 
+    {
         return c != ' ';
     }
 }
